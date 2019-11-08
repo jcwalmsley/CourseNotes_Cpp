b1e93b3e583048a5620223ef55740495316f8f68
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-08 11:10:26 +0100

    Modified multiple lines of text

--- n1.cpp
+++ n1.cpp
@@ -29,3 +29,136 @@ or
 */
 // end of page one
 //--------
+
+/*
+C++ programs have at least two sections.
+Section one is the preprocessing directive.
+A (# hash symbol) is used to indicate the start of a preprocessing directive.
+The # symbol is followed by a keyword wrapped in either <..> or “..” which tells the compiler where to look for the preprocessing directives.
+
+The wrappers are either “...” double quotation marks or <...> double angle brackets
+
+Double quotation marks “...” tells the compiler to look for the specified library in the current directory first and if not found there, then look in the standard (std.) libraries.
+
+Double angle brackets  <...> tells the compiler to look for the preprocessing directory in the location of the standard libraries.
+
+Examples:
+
+	#include “iostream”
+#include // use preprocessing directives of the library indicated by the keyword that follows.
+
+#include “iostream” // search for the preprocessing directive file in the current directory first and if it’s not found there, search for it in the standard library
+
+or
+
+#include <iostream> // search for this preprocessing directive in the standard library
+Section two is the main part of the program:
+Specify the data type returned by the program.
+
+	Example:
+#include “iostream”
+int main()  // this line specifies the data type of the output of the program
+{
+	...some code…;
+	...some code…;
+return 0; //  note: the specified data type on line 2 above must match data type of the output defined here on the last line of code excluding the closing curly brace
+}
+NOTE: the return value 0, an integer, matches the “int main() data type int statement above it. If the data types of these two statements don’t match, the compiler returns an error statement indicating mismatching data types.
+The declaration of the data type is followed by opening and closing curly braces, which indicate the actual start and end of the program. The actual program lies between these two curly braces.
+
+Example 1:
+	#include “iostream”
+int main() { 	// the main section of the program starts here
+	...some code…;
+	...some code…;
+	return 0;
+}    // the main section of the program ends here
+
+or
+Example 2:
+#include <iostream>
+int main()
+{    // the main section of the program starts here
+...some code…;
+	...some code…;
+	return 0;
+}   // the main section of the program ends here
+Each line between the curly braces with a semicolon
+Example:
+#include <iostream>
+int main()
+{
+	…. some code….; // a semicolon signals the end of each line of code
+	…..some code….;
+	return 0;
+}
+
+Writing output to the console:
+
+	Example 1:
+#include <iostream>
+int main()
+{
+	…. some code….;
+	std::cout<<”Hello World”;   // use the command in bold type to print output
+	return 0;
+}
+
+In come cases this format can be used to avoid typing std:: each time.
+
+Example 2:
+#include <iostream>
+using namespace std;  //
+int main()
+{
+	…. some code….;
+cout<<”some text output”; // note std:: can be omitted in this case
+	return 0;
+}
+
+Note: this method might lead to library conflicts on larger projects
+
+Commenting:
+A comment block:
+the comment block goes between these special characters
+/*....some text…….*/
+/*
+Highlighting the lines inside of a comment block can make it nore noticeable as a comment block and can be done using double or triple asterix at the beginning of each indented inner line
+
+Example:
+/* The slash aterix starts the first line of a block quote.
+    ** or *** double or triple asterix can be used for comment blocks occupying more than one line the inner lines can
+
+	 **  Note the ** or *** are completely optional
+
+*/ // asterix slash terminates the block quote
+
+/*
+One line comments:
+// one line comments are indicated by two backslashes at the beginning of the line
+
+Assignment symbol:
+    The =  the equal sign is used when assigning values to a variable
+
+Example:
+Assignment of strings to variables:
+variable name = “some characters” note: wrap stings in double quotes
+
+Assignment of numbers to variables:
+variable name = 20196378  note: numbers aren't enclosed in quotes
+
+Sending output to the console:
+String output:
+	#include <iostream>
+
+############## revise next !!!!!!!!!!!!!!!    
+using namespace std; // note: using the call to namespace here
+int main()
+{
+	…. some code….;
+cout<<”a string produces character output”; // note std:: has been omitted
+std::cout<<”Hello World”; // prints Hello World to the console
+	return 0;
+}
+
+*/
