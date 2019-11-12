060d7585719bf950021f0cfde47d2740beadd6fe
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-12 07:04:48 +0100

    Add file IO section

--- worksheet.cpp
+++ worksheet.cpp
@@ -172,7 +172,7 @@ Ints    Floats    Doubles
 
 // Example three: more extensive formatting example
 
-/*
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -202,9 +202,58 @@ int main()
     cout<< setw(10)<< ccc << "\n";
     return 0;
 }
+
+
+//-----------------
+// File IO steps
+/*
+- Include the <fstream> library
+- Create a stream (input, output, both)
+     - ifstream myfile; (for reading a file)
+     - ofstream myfile; (for writing to a file)
+     - fstream myfile; (for reading and writing a file)
+- Open the file  myfile.open(“filename”);
+- Write or read the file
+- Close the file myfile.close();
 */
 
 
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    string line;
+    /* create an output stream to write to the file append the new lines to the end of the file */
+
+    ofstream myfileI ("input.txt", ios::app);
+    if (myfileI.is_open())
+    {
+        myfileI << "\nI am adding a line.\n";
+        myfileI << "I am adding another line.\n";
+        myfileI.close();
+    }
+    else cout << "Unable to open file for writing";
+
+    //create an input stream to read the file ifstream
+        myfileO ("input.txt");
+    //During the creation of ifstream, the file is opened. So we do not have explicitly open the file.
+    if (myfileO.is_open())
+    {
+        while ( getline (myfileO,line) )
+        {
+            cout << line << '\n';
+        }
+        myfileO.close();
+    }
+
+    else cout << "Unable to open file for reading";
+
+    return 0;
+}
+*/
 
 
 
