d3776d1fee3e225d1d47571d6fb3b2dc38ecfb90
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-13 18:17:06 +0100

    ADD course switch example solution

--- ws.cpp
+++ ws.cpp
@@ -534,6 +534,49 @@ int main()
     }
     return 0;
 }
+
+// alternatively
+
+#include <iostream>
+
+int main()
+{
+    float in1, in2;
+    char operation;
+    float answer;
+
+    std::cout<<"Enter two numbers:\n";
+    std::cin>>in1;
+    std::cin>>in2;
+    std::cout<<"Enter the operation '+','-','*','/':\n";
+    std::cin>>operation;
+
+    switch(operation)
+    {
+        case('+'):  {
+                    answer=in1 + in2;
+                    break;
+                    }
+         case('-'):  {
+                    answer=in1 - in2;
+                    break;
+                    }
+        case('*'):  {
+                    answer=in1 * in2;
+                    break;
+                    }
+        case('/'):  {
+                    answer=in1 / in2;
+                    break;
+                    }
+        default:
+                    std::cout<<"Illegal operation";
+    }
+
+    std::cout<<in1<<operation<<in2<<" = "<<answer<<"\n";
+
+    return 0;
+}
 */
 //-----------------
 
