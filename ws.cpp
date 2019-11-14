67b12fb476a5c4af2e6d1a7c3ee8379f479e1521
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-14 16:32:39 +0100

    ADD section on infinite for loops

--- ws.cpp
+++ ws.cpp
@@ -930,8 +930,26 @@ You guessed the target!
 
 Exit code: 0 (normal program termination)
 */
-
-
+//-----------------
+// Infinite loops:
+/*
+    ** Sometimes it is desired to create an infinite loop.
+    ** For example, in embedded systems a infinite loop is
+    ** often used for the main task.
+*/
+//
+// To create an infinite loop using a for loops
+for( ; ;)
+{
+     std::cout<<"This for loop will run forever\n";
+}
+//
+// To create an infinite loop using a while loop:
+while(1)
+{
+     std::cout<<"This while loop will run forever\n";
+}
+//
 
 
 
