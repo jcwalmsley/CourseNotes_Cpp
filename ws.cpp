01e368fb17f92b04f98fcfed3db15c930d5002e4
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-14 15:16:20 +0100

    ADD output to alternative solution using the random number generator

--- ws.cpp
+++ ws.cpp
@@ -867,8 +867,69 @@ int main()
     ** Guess a number between 0 and 100: 55
     ** Your guess 55 was correct. Congratulations!
 */
+//
 
 
+
+// This alternate solution uses a random number generator to determine a new random target each time the program is executed.
+
+
+#include <iostream>
+#include <sstream>
+#include <time.h> //added for the random number generator seed
+#include <cstdlib>//added to use the rand function
+
+int main()
+{
+    int target;
+    std::string userString;
+    int guess = -1;
+
+    srand(time(NULL));  //set the seed for the random number generator
+    target = rand() %100 + 1; //generate the 'random' number
+
+    while(guess != target)
+    {
+        std::cout<<"Guess a number between 0 and 100: ";
+        std::getline (std::cin,userString);
+        //convert to an int
+        std::stringstream(userString) >> guess;
+        std::cout<<userString<<"\n";
+        if(guess > target)
+            std::cout<<"Your guess is too high\n";
+        else if(guess < target)
+            std::cout<<"Your guess is too low\n";
+        else
+            std::cout<<"You guessed the target!\n";
+
+        //Note I had to use double quotes around "q"
+        //because it is a string
+        if(userString == "q")
+        {
+            std::cout<<"good bye!";
+            std::cout<<"The number was "<<target<<"\n";
+            break;
+        }
+    }
+    return 0;
+}
+//
+/*
+Guess a number between 0 and 100: 50
+50
+Your guess is too low
+Guess a number between 0 and 100: 75
+75
+Your guess is too low
+Guess a number between 0 and 100: 87
+87
+Your guess is too high
+Guess a number between 0 and 100: 80
+80
+You guessed the target!
+
+Exit code: 0 (normal program termination)
+*/
 
 
 
