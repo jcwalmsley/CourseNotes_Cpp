72e691aa5921ce495c4762dbc628288fda3a2170
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-24 16:21:49 +0100

    ADD t1.cpp file to repo

---
+++ M1/t1.cpp
@@ -0,0 +1,22 @@
+/* It's a good idea to become familiar with C++ style guides for code formatting
+    a) modern C++ Coding Guidelines
+    b) Google C++ Style Guidelines
+*/
+
+//-----------------
+// Example 1: C++ Hello World program
+/*
+    "Write a C++ program that outputs the following statement:"
+
+    "Hello world, I am ready for C++"
+*/
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world, I am ready for C++";
+    return 0;
+}
+//
