ca11bd06a7106f86df5a8200a2655cc8024cb023
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-26 12:45:19 +0100

    MODIFY tabs redo

--- M4/t3.cpp
+++ M4/t3.cpp
@@ -7,6 +7,7 @@
     The expression: (A and B and C) or (A and( (not B) or (not C))) = Q
 */
 // /*
+
 #include<iostream>
 
 
@@ -18,10 +19,14 @@ int main()
     int B = 0;
     int C = 0;
     int Q;
+
     Q = (A && B && C) || (A && (!B || !C));
 
-    std::cout<<"A\tB\tC\t(A && B && C)\t\t(!B||!C)\t(A&&(!B||!C))\tQ\n";
+    //Use this output format
+    Q = (A && B && C) || (A && (!B || !C));
+    std::cout<<"A\tB\tC\t(A && B && C)\t\(!B||!C)\t\(A&&(!B||!C))\t\tQ\n";
     std::cout<<A<<"\t"<<B<<"\t"<<C<<"\t\t"<<(A && B && C)<<"\t\t"<<(!B||!C)<<"\t\t"<<(A&&(!B||!C))<<"\t\t"<<Q<<"\n";
+
 
     C = 1;
     Q = (A && B && C) || (A && (!B || !C));
@@ -42,6 +47,7 @@ int main()
     A = 1;
     Q = (A && B && C) || (A && (!B || !C));
     std::cout<<A<<"\t"<<B<<"\t"<<C<<"\t\t"<<(A && B && C)<<"\t\t"<<(!B||!C)<<"\t\t"<<(A&&(!B||!C))<<"\t\t"<<Q<<"\n";
+
 
     C = 1;
     B = 0;
