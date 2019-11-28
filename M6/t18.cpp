bd8d87dd86c3350f6156a9668c2ce45262066cc6
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-28 18:30:54 +0100

    MODIFY code on line 30

--- M6/t18.cpp
+++ M6/t18.cpp
@@ -9,7 +9,7 @@
 
 int main() {
     int userInput = 0;
-    int minNumber = 0;
+    int minNumber = 100;
     int maxNumber = 0;
     int sumTotal  = 0;
     float average = 0;
@@ -19,18 +19,19 @@ int main() {
         std::cout<<"Enter a number: ";
         std::cin>>userInput;
         std::cout<< userInput <<"\n";
+        }
+        if (userInput > maxNumber) {
+            maxNumber = userInput;
+        }
+        if (userInput < minNumber) {
+            minNumber = userInput;
+        }
+        sumTotal = sumTotal + userInput;
     }
-    if (userInput > maxNumber) {
-        maxNumber = userInput;
-    }
-    if (userInput < minNumber) {
-        minNumber = userInput;
-    }
-    sumTotal = sumTotal + userInput;
     std::cout<< "Maximum number = " << maxNumber << "\n";
     std::cout<< "Minimum number = " << minNumber << "\n";
     std::cout<< "sumTotal = " << sumTotal << "\n";
-    average = sumTotal / 15.0;
+    average = (sumTotal / 15.0);
     std::cout<< "Average = " << average << "\n";
     return 0;
 }
