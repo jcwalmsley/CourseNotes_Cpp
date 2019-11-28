abd792ac1622fa177e37502673c03c20e7e1026a
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-28 15:09:24 +0100

    ADD main.cpp file

---
+++ M5/P1/main.cpp
@@ -0,0 +1,77 @@
+/*
+    Program exercise:
+    For each variable print the value of the variable,
+    then print the address where it is stored.
+*/
+/*
+#include<iostream>
+#include<string>
+
+int main()
+{
+    int givenInt;
+    float givenFloat;
+    double givenDouble ;
+    std::string givenString;
+    char givenChar;
+
+    return 0;
+}
+*/
+// Solution to programming exercise:
+//
+#include<iostream>
+#include<string>
+
+int main()
+{
+    int givenInt;
+    float givenFloat;
+    double givenDouble ;
+    std::string givenString;
+    char givenChar;
+    int *pointerGivenInt;
+    int **pionterPointerGivenInt;
+
+    pointerGivenInt = &givenInt;
+    pointerPointerGivenInt = &pointerGivenInt;
+
+    // return values of variables
+
+    std::cout<<"interger = \n";
+    std::cin>> givenInt;
+    std::cout<<"float = \n";
+    std::cin>>givenFloat;
+    std::cout<<"double = \n";
+    std::cin>>givenDouble;
+
+    // using cin.ignore so that the leftover characters in the buffer left over from givenDouble will be ignored.
+
+    std::cin.ignore();
+    std::cout<<"character = \n";
+    std::cin>>givenChar;
+
+    std::cout<<"string = \n";
+    std::cin.ignore();
+    std::getline (std::cin,givenString);
+
+    // values
+    std::count<<"interger = "<<givenInt<<"\n";
+    std::cout<<"float = "<<givenFloat<<"\n";
+    std::Cout<<"double ="<<givenDouble<<"\n";
+    std::cout<<"string = "<<givenString<<"\n";
+    std::cout<<"character = "(char)givenChar<<"\n";
+
+    // memory addresses
+    std::cout<<"address integer ="<<&givenInteger<<"\n";
+    std::cout<<"address float = "<<&givenFloat<<"\n";
+    std::cout<<"address double ="<<&givenDouble<<"\n";
+    std::cout<<"address string = "<<&givenString<<"\n";
+    std::cout<<"address character = "<<&givenChar<<"\n";
+
+    // get values stored using indirection
+    std::cout<<"pointer of givenInt = "<<*pointerGivenInt<<"\n";
+    std::cout<<"pointer of pointer of givenInt = "<<**pointerPointerGivenInt<<"\n";
+    
+    return 0;
+}
