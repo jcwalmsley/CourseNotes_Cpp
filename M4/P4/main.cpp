b37fe9603a99032f49bdf798c2db941e654905f3
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-11-29 23:20:17 +0100

    MODIFY 2 files main.cpp main.hpp

--- M4/P4/main.cpp
+++ M4/P4/main.cpp
@@ -3,16 +3,18 @@
     Print out the sum and average of the five numbers.
 */
 //
-#include <iostream>
 
 int main()
 {
+    std::string input;
     float input;
     float sum;
 
     for(int i=0;i<5;i++)
     {
         std::cout<<"Enter five numbers?\n";
+        std::getline (std::cin, input);
+        std::stringstream(string) >> input;
         std::cin>>input;
         sum = sum + input;
     }
