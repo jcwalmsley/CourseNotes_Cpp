57b00c595b4478db76b6c4485ef63fe35a55b337
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-01 12:15:05 +0100

    UPDATE file main.cpp output.txt

--- M4/P2e/main.cpp
+++ M4/P2e/main.cpp
@@ -1,12 +1,13 @@
 /*
-    Additional examples and potential problems
+    Additional examples with explanations:
 */
 #include <iostream>
 
 int main()
 {
     int i = 2;
-    switch (i) {
+    switch (i)
+    {
         case 1: std::cout << "1";
         case 2: std::cout << "2";   //execution starts at this case label
         case 3: std::cout << "3";
@@ -18,7 +19,8 @@ int main()
 
     std::cout << '\n';
 
-    switch (i) {
+    switch (i)
+    {
         case 4: std::cout << "a";
         default: std::cout << "d"; //there are no applicable constant_expressions
                                    //therefore default is executed
@@ -26,40 +28,54 @@ int main()
 
     std::cout << '\n';
 
-    switch (i) {
+    switch (i)
+    {
         case 4: std::cout << "a";  //nothing is executed
     }
 
-    // when enumerations are used in a switch statement, many compilers
-    // issue warnings if one of the enumerators is not handled
+    /*
+        when enumerations are used in a switch statement, many compilers
+        issue warnings if one of the enumerators is not handled
+    */
 
     enum color {RED, GREEN, BLUE};
-    switch(RED) {
+    switch(RED)
+    {
         case RED:   std::cout << "red\n"; break;
         case GREEN: std::cout << "green\n"; break;
         case BLUE:  std::cout << "blue\n"; break;
     }
-    //    the C++17 init-statement syntax can be helpful when there is
-    //    no implicit conversion to integral or enumeration type
-    //    switch (Device dev = get_device(); dev.state()).
-
+    //
+    /*
+        the C++17 init-statement syntax can be helpful when there is
+        no implicit conversion to integral or enumeration type
+    */
+    /*
+    switch (Device dev = get_device(); dev.state()) // Error: Device & dev not declared
     {
-       case SLEEP: /*...*/ break;
-       case READY: /*...*/ break;
-       case BAD: /*...*/ break;
+        case SLEEP: /.../ break;
+        case READY: /.../ break;
+        case BAD: /.../ break;
     }
-    //  pathological examples, the statement doesn't have to be a
-    //  compound statement
+    */
+    /*
+        pathological examples, the statement doesn't have to
+        be a compound statement
+    */
+    /*
     switch(0)
+    {
         std::cout << "this does nothing\n";
-
+    }
+    */
     // labels don't require a compound statement either
     switch(int n = 1)
+    {
         case 0:
         case 1: std::cout << n << '\n';
+    }
     // Duff's Device: http://en.wikipedia.org/wiki/Duff's_device
 }
-*/
 
 /* Output:
     2345
