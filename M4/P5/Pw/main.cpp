30ba7594f6de13f05d3f129d3822e2d7d74f6610
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-02 10:28:20 +0100

    UPDATE this dir with main.cpp

--- M4/P5/Pw/main.cpp
+++ M4/P5/Pw/main.cpp
@@ -1,63 +1,30 @@
 /*
-    Programming exercise:
-
-    Use a while loop to prompt the user to guess a target
-    number between 0 and 100.
-
-    Message user if the guess is too high or too low.
-
-    If the user enters -1  or guesses the target number
-    exit the program.
-
+    Example syntax of while loops
 */
-/*
-Information given for exercise:
 
 #include <iostream>
-#include<sstream>
 
 int main()
 {
-    //use 55 as the number to be guessed
-    int target = 55;
-    int guess = -1;
-    std::cout<<"Guess a number between 0 and 100: ";
-    std::cin>>guess;
-    std::cout<<guess<<"\n";
+    int entry = 0;
 
+    //with this while loop the condition is true
+    //so the statements are executed
 
-    return 0;
-}
-*/
-// provided solution:
-
-#include <iostream>
-#include<sstream>
-
-int main()
-{
-    //use 55 as the number to be guessed
-    int target = 55;
-    int guess = -1;
-
-while(guess != target )
+    while(entry <=5)
     {
-        std::cout<<"Guess a number between 0 and 100: ";
-        std::cin>>guess;
-        std::cout<<guess<<"\n";
-        if(guess > target)
-            std::cout<<"Your guess is too high\n";
-        else if(guess < target)
-            std::cout<<"Your guess is too low\n";
-        else
-            std::cout<<"You guessed the target!\n";
-
-        if(guess == -1)
-        {
-            std::cout<<"good bye!";
-            break;
-        }
+        std::cout<<"incrementing entry = "<<entry<<"\n";
+        entry++;
     }
+
+    //with this while loop the condition is false
+    //so the statements are not executed
+
+    while(entry < 5)
+    {
+        std::cout<<"decrementing entry = "<<entry<<"\n";
+        entry--;
+    }
+
     return 0;
 }
-//
