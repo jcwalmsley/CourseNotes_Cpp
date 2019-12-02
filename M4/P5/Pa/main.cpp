1e28cbf590dceae976461fa110dbe28c340b0a80
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-02 09:14:53 +0100

    RENAME and add 2 files to dir Pa/

---
+++ M4/P5/Pa/main.cpp
@@ -0,0 +1,57 @@
+/*
+    Programming exercise:
+    Use a while loop to prompt the user to guess a target
+    number between 0 and 100. Message the user if the guess
+    is too high or too low. If the user enters -1  or guesses
+    the target number exit the program.
+
+*/
+/* Given information for this exercise:
+#include <iostream>
+#include<sstream>
+
+int main()
+{
+    //use 55 as the number to be guessed
+    int target = 55;
+    int guess = -1;
+    std::cout<<"Guess a number between 0 and 100: ";
+    std::cin>>guess;
+    std::cout<<guess<<"\n";
+
+
+    return 0;
+}
+// solution from course instruction:
+*/
+
+#include <iostream>
+#include<sstream>
+
+int main()
+{
+    //use 55 as the number to be guessed
+    int target = 55;
+    int guess = -1;
+
+while(guess != target )
+    {
+        std::cout<<"Guess a number between 0 and 100: ";
+        std::cin>>guess;
+        std::cout<<guess<<"\n";
+        if(guess > target)
+            std::cout<<"Your guess is too high\n";
+        else if(guess < target)
+            std::cout<<"Your guess is too low\n";
+        else
+            std::cout<<"You guessed the target!\n";
+
+        if(guess == -1)
+        {
+            std::cout<<"good bye!";
+            break;
+        }
+    }
+    return 0;
+}
+//
