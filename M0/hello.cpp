af4663441be12395aa4fb96110f583e161da51c2
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-04 09:01:50 +0100

    RENAME dir M1/ to M0/

---
+++ M0/hello.cpp
@@ -0,0 +1,16 @@
+/*
+    Program "Hello World" example:
+
+    Write a program that returns:
+
+    "Hello world, let's learn C++"
+*/
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world, let's learn C++";
+    return 0;
+}
+//
