c9aab6ba170cf3c39a2fe01f937699292cdd7f68
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-06 09:47:27 +0100

    RENAME dir M0/ to M1_intro/

---
+++ M1_intro/const_var.cpp
@@ -0,0 +1,15 @@
+/* Example 5: define constants
+    "Use constant variables'
+*/
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int weightGoal = 100;
+    cout<<"WeightGoal = "<<weightGoal<<"\n";
+    weightGoal = 200;
+    cout<<"WeightGoal = "<<weightGoal<<"\n";
+    return 0;
+}
