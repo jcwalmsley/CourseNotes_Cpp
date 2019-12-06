2735553e39b8381f9ed5550550a7670df567288a
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-06 10:45:41 +0100

    REMOVE file fun_ex1

--- M7_functions/fun_ex1.cpp
+++
@@ -1,27 +0,0 @@
-/*
-    "Now, it's your turn to try. Write a program that uses a function called, "printMessage".
-
-    The function should print the message, "Functions". Print this message with the same capitalization and punctuation, the grader will be checking for them.""
-*/
-
-/*
-    "Goal: write a function called printMessage()
-    **that prints: Functions"
-*/
-
-#include<iostream>
-
-void printMessage();
-
-int main()
-{
-    printMessage();
-
-    return 0;
-}
-
-void printMessage()
-{
-    std::cout<<"Functions";
-}
-  
