9047acf1d0b58d1cec89db07cb93d0fd4c2cfd42
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-06 13:07:16 +0100

    MODIFY comment text and format

--- M7_functions/introduction.cpp
+++ M7_functions/introduction.cpp
@@ -1,46 +1,58 @@
 /*
-C++ supports functions, in fact main() is nothing more than a special C++ function.
+    Functions are a component of C++ programs
+    The most common function in C++ is the main() function which is the only
+    function that doesn't require a function declaration therefore it is a special
+    function in C++.
 
-All C++ functions (except the special case of the main function) must have:
+    A function delcaration describes how to call a given function.
 
-A declaration: this is a statement of how the function is to be called.
-A definition: this is the statement(s) of the task the function performs when called
-C++ functions can:
+    A function definition describes what the function will do when called.
 
-Accept parameters, but they are not required
-Return values, but a return value is not required
-Can modify parameters, if given explicit direction to do so
-Function Syntax:
+    C++ functions are able to, but not required to include:
+        accept parameters
+        return values
+        modify parameters 'when given the method of how to'
+*/
+
+//  Syntax of functions:
 
 retVariableType  functionName(parameter1, parameter2, ...,parameterN)
 {
      statement(s);
 }
-For example:
-
+//
+//  For example:
+/*
+    The followoign function accepts no parameters and returns no value.
+    When it is called in a program, it will execute the statement
+*/
+//
 void printValues()
 {
-     std::cout<<"Hello World!";
+    std::cout<<"Hello World!";
 }
-This function accepts no parameters and returns no value. When it is called in a program, it will execute the statement
+// Output:
 
 std::cout<<"Hello World!";
-Function Declaration: A function is declared with the syntax:
 
+//-----------------
+//  Syntax of declarations: note declaration ends with semi-colons.
+
+//  Example:
 returnVariableType  functionName(parameter1, parameter2, ...,parameterN);
 
-Note the semi-colon at the end of the statement.
+//-----------------
+/*
+    Calling functions:
 
-Calling the function
-
-Functions are called using their function name with any parameters specified in the declaration and definition.
-
-For example:
-
+    Call functions with the function name, including any parameters specified in the
+    declaration and function definition in parenthesis separated by commas.
+*/
+//  For example:
+//
 int main()
 {
      ...
      functionName(parameter1, parameter2, ..., parameterN);
      ...
 }
-*/
