7d71640ad07d8509024797ced24293c8f3cd9c72
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-06 17:25:05 +0100

    MODIFY lines of text in the block comments added new block comments

--- M7_functions/fun_header_ex.cpp
+++ M7_functions/fun_header_ex.cpp
@@ -1,5 +1,5 @@
 /*
-    Putting functions and function declarations in a header file often
+    Putting function definitions and declarations in a header file often
     makes sense because; the main function looks better organized, and
     is easier to understand by giving the overall code a cleaner appearance.
 
@@ -8,14 +8,18 @@
     main.cpp // main program file
     main.hpp // header file to the main program file
 
-    When using a .h or .hpp header file include the following statement
+    When using a .h or .hpp header file, include the following statement
     in the main.cpp file:
 
-    #include "main.hpp"
+    #include "main.hpp" /* when the program compiles the linker will link
+                        main.cpp with the printProduct() function declaration
+                        and definition located in the main.hpp file so that
+                        the program will execute. */
 */
 /*
     The example below is a non executing program referencing a header file containing
     the printProduct() function declaration and printProduct() function definition.
+
     This is what the main.cpp program would look like if it had an existing main.hpp
     file in the same directory that included the function declaration and definition
     for the printProduct() function.
