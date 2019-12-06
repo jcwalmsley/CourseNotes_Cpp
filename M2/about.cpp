5654ae273ffe206f012296fd682d94d5862e0581
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-06 09:40:11 +0100

    REMOVE dir M2/

---
+++ M2/about.cpp
@@ -0,0 +1,28 @@
+/*
+    "Arithmetic operations:
+    "When doing math operations you may need to include the cmath library,
+    as it contains a number of useful functions."
+    #include <cmath>
+
+    A particularly useful math function is the power function:
+        Format:
+        std::pow(base, exponent);
+*/
+//
+#include<cmath>
+//
+M_PI = 3.14159265358979323846 //  "M_PI is equivalent to pi"
+//
+//-----------------
+// Format of pow format: 3^5 is equal to std::pow(3, 5)
+std::pow(3, 5);
+//
+/*
+    "Exercise; Calculate the volumes of:
+        a cube, sphere, cone.
+        Cube Volume = side^3
+        Sphere Volume = (4/3) * pi * radius^3
+        Cone Volume = pi * radius^2 * (height/3)
+        Write the values to the console."
+*/
+//-----------------
