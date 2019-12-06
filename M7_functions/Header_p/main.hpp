c6c1c902e0013d01afcda3990d4079e17592d5f8
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-06 20:17:47 +0100

    MODIFY function declaration and definition

--- M7_functions/Header_p/main.hpp
+++ M7_functions/Header_p/main.hpp
@@ -1,19 +1,11 @@
 #include <iostream>
 
-//function 1 declaration: including declarations in .hpp is optional
-void printEquation(int n1, int n2, int sum, char operation);
-//function 2 declaration
-int sum(int n1, int n2);
+//function declaration: including declarations in .hpp is optional
+void printProduct(int m1, int m2, int product);
 //-----------------
 
-//function 1 definition:
-void printEquation(int n1, int n2, int sum, char operation)
+//function definition:
+void printProduct(int m1, int m2, int product)
 {
-    print the equation 'n1 operation  n2 = result'
-}
-
-//function 2 definition
-int sum(int n1, int n2)
-{
-    add two integers and return the sum
+    std::cout << m1 <<"*"<< m2 <<" = "<<product << " \n";
 }
