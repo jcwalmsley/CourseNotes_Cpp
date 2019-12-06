1a70e7f81693774684eb2459ca3ba500730e5fbf
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-06 20:02:35 +0100

    RENAME both fun_header files to main files

---
+++ M7_functions/Header_p/main.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+//function 1 declaration:
+void printEquation(int n1, int n2, int sum, char operation);
+
+//function 1 definition:
+void printEquation(int n1, int n2, int sum, char operation)
+{
+    print the equation 'n1 operation  n2 = result'
+}
+
+//-----------------
+
+//function 2 declaration
+int sum(int n1, int n2);
+
+//function 2 definition
+int sum(int n1, int n2)
+{
+    add two integers and return the sum
+}
