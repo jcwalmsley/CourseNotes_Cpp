4c0f943d0a948487f9d9ffc7f9861641b7a48f61
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-06 10:04:55 +0100

    ADD fun_base_ex.cpp

---
+++ M7_functions/fun_base_ex.cpp
@@ -0,0 +1,24 @@
+/*Now, it's your turn to try. Write a program that uses a function called, "printMessage".
+
+The function should print the message, "Functions". Print this message with the same capitalization and punctuation, the grader will be checking for them.
+*/
+/*Goal: write a function called printMessage()
+**that prints: Functions
+*/
+
+#include<iostream>
+
+void printMessage();
+
+int main()
+{
+    printMessage();
+
+    return 0;
+}
+
+void printMessage()
+{
+    std::cout<<"Functions";
+}
+  
