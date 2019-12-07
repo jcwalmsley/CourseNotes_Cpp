08ae8e7390273b14c3759ee4af92910aed9f10ec
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-07 14:58:46 +0100

    ADD 1 file main.hpp

---
+++ M7_functions/Return_p/p2/main.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//-----------------
+
+// function declaration 1
+void printEquation(int n1, int n2, int sum, char operation);
+
+// function declaration 2
+int sum(int n1, int n2);
+
+//-----------------
+
+// function definition 1
+void printEquation(int n1, int n2, int sum, char operation)
+{
+     print the equation 'n1 operation  n2 = result'
+}
+
+// function definition 2
+int sum(int n1, int n2)
+{
+     add two integers and return the sum
+}
