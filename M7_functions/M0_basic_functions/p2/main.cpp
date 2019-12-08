8dd682d0714f3c76910c481d3a7f6f6c29e6c1a2
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-08 15:12:04 +0100

    RENAME dir Intro_p/ to M0_basic_functions

---
+++ M7_functions/M0_basic_functions/p2/main.cpp
@@ -0,0 +1,20 @@
+/*
+    Write a program using a basic function (ie., no parameters or return values)
+    that prints some string output to console.
+*/
+
+#include<iostream>
+
+void printMessage();
+
+int main()
+{
+    printMessage();
+
+    return 0;
+}
+
+void printMessage()
+{
+    std::cout<<"A basic function has no parameters or return values, yet can print strings to console.";
+}
