7be82cd6dd752f9b3571e165ec55d94aee98090c
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-08 15:42:21 +0100

    ADD dir p2 with 3 files main.cpp & out.txt & main

---
+++ M7_functions/M1_fun_w_param/p2/fun_param_ex.cpp
@@ -0,0 +1,35 @@
+/*
+    Given a function printProduct().
+
+    Write the function declaration and definition.
+
+    The function has two parameters; n1 & n2.
+
+    It prints the message:
+    "n1 * n2 = product"
+
+    And returns:
+    3 * 8 = 24
+*/
+//
+#include <iostream>
+
+int printProduct(int n1, int n2, int product);
+
+int main()
+{
+    int n1 = 3;
+    int n2 = 8;
+    int product;
+
+    product = n1 * n2;
+
+    printProduct(n1, n2, product);
+
+    return 0;
+}
+
+int printProduct(int n1, int n2, int product)
+{
+    std::cout<<n1<< " * " <<n2<< " = " <<product<<"\n";
+}
