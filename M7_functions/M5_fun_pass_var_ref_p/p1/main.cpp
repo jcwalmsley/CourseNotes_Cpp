b139012774c0a76ca14c6efbc65206274dc85d7c
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-08 17:26:19 +0100

    MOVE 2 subdirs to new parent dir

---
+++ M7_functions/M5_fun_pass_var_ref_p/p1/main.cpp
@@ -0,0 +1,29 @@
+/*
+    By referencing a variables address in a function rather than referencing
+    the value of the variable stored there we can modify the value that the
+    address points to, rather than the actual variable saved in the location.
+*/
+
+#include<iostream>
+
+void increment(int &input); //"&" gives the address of the input rather than the value
+
+int main()
+{
+    int a = 31;
+    std::cout<<"Prior to function call a = "<<a<<"\n";
+    increment(a);
+    std::cout<<"Following function call a = "<<a<<"\n";
+    return 0;
+}
+void increment(int &input)//Note the addition of '&'
+{
+    input++; //**Note the LACK OF THE addition of '&'**
+    std::cout<<"During function call a = "<<input<<"\n";
+}
+/*
+    Output
+    Prior to function call a = 31
+    During function call a = 32
+    Following function call a = 32
+*/
