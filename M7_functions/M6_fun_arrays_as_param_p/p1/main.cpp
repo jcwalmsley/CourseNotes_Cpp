10f3b8f8ba52420b82f9e68cbef41cb6dbef1921
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-09 06:13:50 +0100

    MODIFY block comment and made use of arrayName consistent thoughout file

--- M7_functions/M6_fun_arrays_as_param_p/p1/main.cpp
+++ M7_functions/M6_fun_arrays_as_param_p/p1/main.cpp
@@ -1,8 +1,8 @@
 /*
-    Even though it isn't possible to pass an array to a function.
-    It is possible to pass an array by reference to a function.
-    This can be done in 3 ways.
+    Even though it's not possible to pass arrays to functions,
+    arrays can be passed by reference to a function.
 
+    3 methods of passing arrays to functions:
     void functionName(variableType *arrayName)
     void functionName(variableType arrayName[length of array])
     void functionName(variableType arrayName[])
@@ -16,49 +16,49 @@
 #include<iomanip>
 
 //As a pointer
-void arrayAsPointer(int *array, int size);
+void arrayAsPointer(int *arrayName, int size);
 
 //As a sized array
-void arraySized(int array[5], int size);
+void arraySized(int arrayName[5], int size);
 
 //As an unsized array
-void arrayUnSized(int array[], int size);
+void arrayUnSized(int arrayName[], int size);
 
 int main()
 {
     const int size = 6;
-    int array[size] = {11,22,55,44,66,77};
+    int arrayName[size] = {11,22,55,44,66,77};
         /*
             When passing pointers or references to arrays
             we often don't know the array's size therefore,
             pass size also.
         */
-    arrayAsPointer(array, size);
-    arraySized(array, size);
-    arrayUnSized(array, size);
+    arrayAsPointer(arrayName, size);
+    arraySized(arrayName, size);
+    arrayUnSized(arrayName, size);
     return 0;
 }
 
-void arrayAsPointer(int *array, int size)
+void arrayAsPointer(int *arrayName, int size)
 {
     std::cout<<std::setw(5);
     for(int i=0; i<size; i++)
-        std::cout<<array[i]<<" ";
+        std::cout<<arrayName[i]<<" ";
     std::cout<<"\n";
 }
 
-void arraySized(int array[6], int size)
+void arraySized(int arrayName[6], int size)
 {
     std::cout<<std::setw(5);
     for(int i=0; i<size; i++)
-        std::cout<<array[i]<<" ";
+        std::cout<<arrayName[i]<<" ";
     std::cout<<"\n";
 }
 
-void arrayUnSized(int array[], int size)
+void arrayUnSized(int arrayName[], int size)
 {
     std::cout<<std::setw(5);
     for(int i=0; i<size; i++)
-        std::cout<<array[i]<<" ";
+        std::cout<<arrayName[i]<<" ";
     std::cout<<"\n";
 }
