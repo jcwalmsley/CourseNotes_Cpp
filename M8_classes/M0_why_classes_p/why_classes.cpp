e70d99b731be0fb68ddeb37a1aa3f698a61a2006
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-10 11:19:20 +0100

    MODIFY block comment

---
+++ M8_classes/M0_why_classes_p/why_classes.cpp
@@ -0,0 +1,42 @@
+/*
+    Can you think of a problem that classes can solve?
+    This program will attempt to store student information.
+    Write this program such that it sorts the student data being stored by:
+        Student Name
+        Student Number
+        Student Graduation Date
+
+        Information on 5 students is provided.
+
+        Sort students by name, then by number, then by graduation date.
+
+        Print student information after each sort.
+
+    It is not expected that you can successfully write this program but give it
+    your best shot without using classes.
+
+    The following information is provided:
+        student1: Name: Joe Lime, ID#15, Grad. Date: 2019
+        student2: Name: Bob Green, ID#3, Grad. Date: 2020
+        student3: Name: SallyAnne Green , ID#1, Grad. Date: 2017
+        student4: Name: Annie Blue, ID#10, Grad. Date: 2020
+        student5: Name: Jose Lemon, ID#25, Grad. Date: 2016
+
+    Use the general format:
+    main.cpp
+    main.hpp
+*/
+
+
+
+#include"main.hpp"
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    //sort by name, then by number, then by gradDate
+    return 0;
+}
+  
