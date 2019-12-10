4a6a76ababe3be6b3ef9c2cb4b93724ec48ccd9b
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-10 12:56:58 +0100

    UPDATE class_members.cppp add cp_cl_mem.cpp

---
+++ M8_classes/M1_classes/p1/cp_cl_mem.cpp
@@ -0,0 +1,18 @@
+//practice writing the code for this program
+void setName(std::string nameIn)
+//
+class student
+{
+        string name;
+        int id;
+        int gradDate;
+
+    public:
+        void setName(string name);
+        void setId(int idIn);
+        void setGradDate(int dateIn);
+        string getName();
+        int getId();
+        int getGradDate();
+        void print();
+};
