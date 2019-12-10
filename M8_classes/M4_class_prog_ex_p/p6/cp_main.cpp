fe1ec8c355d6d5df650aed3ffa0c192be3d33638
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-10 16:29:21 +0100

    ADD cp_main.cpp, cp_main.hpp

---
+++ M8_classes/M4_class_prog_ex_p/p6/cp_main.cpp
@@ -0,0 +1,41 @@
+/*
+    In this program create a class called Cats.
+
+    It has three private members: name, breed, age.
+
+    Create set and get and printInfo functions:
+        private members:
+            name, breed, age
+        public members:
+            setName, setBreed,setAge
+            getName, getBreed, getAge,
+            printInfo.
+
+    main.cpp
+    main.hpp
+*/
+//-----------------
+// A sample main.cpp program
+//
+#include"main.hpp"
+
+int main()
+{
+    Cat cat1,cat2;
+    cat1.setName("Kimmy");
+    cat2.setName("Bobby");
+    cat1.setBreed("calico");
+    cat2.setBreed("main coon");
+    cat1.setAge(4);
+    cat2.setAge(1);
+
+    cat1.printInfo();
+    cout<<"\n";
+    cat2.printInfo();
+    cout<<"\n\n";
+    //Alternate printing method
+    cout<<cat1.getName()<<" "<<cat1.getBreed()<<" "<<cat1.getAge()<<"\n";
+    cout<<cat2.getName()<<" "<<cat2.getBreed()<<" "<<cat2.getAge();
+    return 0;
+}
+  
