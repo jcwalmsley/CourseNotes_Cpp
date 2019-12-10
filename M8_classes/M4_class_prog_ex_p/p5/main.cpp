19fc2d1ab93301d63d2a738118c989d03f329813
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-10 07:14:41 +0100

    UPDATE main.cpp & main.hpp

--- M8_classes/M4_class_prog_ex_p/p5/main.cpp
+++ M8_classes/M4_class_prog_ex_p/p5/main.cpp
@@ -12,12 +12,31 @@
     assign license #8765 to dog1
     assign license #2851 to dog2
 */
-
-
+//
 #include"main.hpp"
 
 int main()
 {
 
+    return 0;
+}
+//
+/*
+    A sample solution of the main.cpp program.
+*/
+//
+#include"main.hpp"
+
+int main()
+{
+    Dog dog1, dog2;
+    dog1.setName("Trixie");
+    dog2.setName("Kali");
+    dog1.setLicenseNumber(1234);
+    dog2.setLicenseNumber(5678);
+
+    dog1.printInfo();
+    cout<<"\n";
+    dog2.printInfo();
     return 0;
 }
--- M8_classes/M4_class_prog_ex_p/p5/main.hpp
+++ M8_classes/M4_class_prog_ex_p/p5/main.hpp
@@ -1,5 +1,4 @@
-/*The header file for main.cpp*/
-
+//
 #include<iostream>
 
 using namespace std;
@@ -39,4 +38,53 @@ string getLicenseNumber()
 void printInfo()
 {
     //ToDo: finish this function
+}
+//
+//-----------------
+/*
+
+    A sample solution for the header file.
+
+*/
+/*The header file for main.cpp*/
+
+#include<iostream>
+
+using namespace std;
+
+class Dog
+{
+    string name;
+    int licenseNumber;
+public:
+    void setName(string nameIn);
+    void setLicenseNumber(int licenseNumberIn);
+    string getName();
+    int getLicenseNumber();
+    void printInfo();
+};
+
+void Dog::setName(string nameIn)
+{
+    name = nameIn;
+}
+
+void Dog::setLicenseNumber(int licenseNumberIn)
+{
+    licenseNumber = licenseNumberIn;
+}
+
+string Dog::getName()
+{
+    return name;
+}
+
+int Dog::getLicenseNumber()
+{
+    return licenseNumber;
+}
+
+void Dog::printInfo()
+{
+    cout<<name<<" "<<licenseNumber;
 }
