2502d0df8a61a70316d9c7ecea5686cfd899fed4
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-11 09:57:51 +0100

    TYPO removed one space

--- M8_classes/M6_helper_fun_p/p9/main.cpp
+++ M8_classes/M6_helper_fun_p/p9/main.cpp
@@ -19,7 +19,7 @@ int main()
     Student s1;
     s1.setId(3232);
     s1.setGrade(0,85);
-    s1.setGrade(1, 80);
+    s1.setGrade(1,80);
     s1.setGrade(2,98);
     s1.setGrade(3,65);
     s1.setGrade(4,90);
