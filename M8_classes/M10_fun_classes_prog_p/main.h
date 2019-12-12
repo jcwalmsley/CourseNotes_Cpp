61702609645a8e5e011a56bf6de0ac433f83f5a8
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-12 15:23:04 +0100

    ADD M10_fun_classes_prog_p to M8_classes/ dir

---
+++ M8_classes/M10_fun_classes_prog_p/main.h
@@ -0,0 +1,19 @@
+//main.h
+
+class Student
+{
+public:
+    int studentID;
+    int grades[10];
+};
+
+//********************
+class CourseEnrollment
+{
+    public:
+    Student students[6];
+    int courseMinGrade;
+    int courseMaxGrade;
+    float courseAvgGrade;
+    int grades[60];
+};
