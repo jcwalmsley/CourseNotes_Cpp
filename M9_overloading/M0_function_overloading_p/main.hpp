06931cff73a47c40c326fd965dcd52f172ea8d1c
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-14 21:56:05 +0100

    ADD main.cpp main.hpp to M0_function_overloading_p/

---
+++ M9_overloading/M0_function_overloading_p/main.hpp
@@ -0,0 +1,48 @@
+Now, it's your turn to use overloading.
+
+Add two more variations on findSmaller;
+
+create functions that accept three arguments for each of the variable types
+create functions that accept an array for each variable type.
+main.cpp
+main.hpp
+#include<iostream>
+
+//C++ allows us to 'overload' the same function name as long as
+//the variable types in the function inputs are different.
+
+class Compare
+{
+public:
+    int findSmaller(int input1, int input2);
+    float findSmaller(float input1, float input2);
+    char findSmaller(char input1, char input2);
+};
+
+int Compare::findSmaller(int input1, int input2)
+{
+    if(input1<input2)
+        return input1;
+    return input2;
+}
+float Compare::findSmaller(float input1, float input2)
+{
+    if(input1<input2)
+        return input1;
+    return input2;
+}
+
+char Compare::findSmaller(char input1, char input2)
+{
+    if(input1<input2)
+        return input1;
+    return input2;
+}
+
+  
+
+
+
+
+
+  
