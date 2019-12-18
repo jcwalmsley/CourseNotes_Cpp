756d77853fdf21385e93c2cf7e8c7ddb48f798f7
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-18 16:15:48 +0100

    RENAME 3 files

---
+++ M10_templates/M0_function_templates_p/main_ex.hpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+
+//to create a generic template function
+//inform the compiler we're using a template of type T
+template <typename T> //template function declaration
+
+template <typename T>
+T sumTwo(T a, T b)
+{
+    T sum;
+    sum = a + b;
+    return sum;
+}
