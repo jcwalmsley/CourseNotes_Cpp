3301f09548ec2c494939cc2c9e4d4c7b7104e749
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-18 06:22:55 +0100

    MODIFY code in the main_ex.hpp file to execute the program

---
+++ M9_overloading/M2_overloading_operators_p/M0_overload_operators_p/main_ex.cpp
@@ -0,0 +1,16 @@
+//
+//#include "main_ex.hpp"
+#include "sol_main.hpp"
+
+int main(void)
+{
+   Shape sh1(4, 4);    // Declare shape1
+   Shape sh2(2, 6);    // Declare shape2
+   Shape sh3;          //Declare shape3
+
+   int total = sh1 + sh2;
+   cout << "\nsh1.Area() = " << sh1.Area();
+   cout << "\nsh2.Area() = " << sh2.Area();
+   cout << "\nTotal = "<<total;
+   return 0;
+}
