3f1d133ce98b95a158b4c2ac0bc19cd945761d22
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-18 16:16:43 +0100

    REMOVE 2 renamed files

--- M10_templates/M0_function_templates_p/main.cpp
+++
@@ -1,19 +0,0 @@
-/*
-    Create a generic function that accepts 2 inputs and returns their sum
-    Put the function declaration and definition in the head file.
-
-    main.cpp
-    main.hpp
-
-    Goal: Practice writing generic functions
-*/
-#include "main.hpp"
-
-int main()
-{
-    int a = 90;
-    int b = 1;
-    std::cout<<a <<" + "<<b<<" = "<< sumTwo(a,b);
-    return 0;
-}
-  
