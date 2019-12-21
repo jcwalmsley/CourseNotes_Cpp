8ac8d2e0056d0d175e51ff170b167f74bcdd7b44
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-21 15:19:19 +0100

    MOVE 4 files into new dir example_1

---
+++ M11_classes_inheritance/M2_multiple_inheritance/example_1/main.cpp
@@ -0,0 +1,26 @@
+/*
+    C++ classes can inherit from more than one class. This is known as "Multiple Inheritance".
+
+    The form for declaring multiple inheritance is:
+
+    class DerivedClass : access BaseClass1, ... ,access BaseClassN
+
+    For example, in the statement shown below, the derived class is TeachingAssociate. It inherits attributes from Staff and from GradStudent.
+
+    class TeachingAssociate: public Staff, public GradStudent
+
+    Let's look at an example of multiple inheritance.
+*/
+/*Goal: understand multiple inheritance*/
+
+#include "main.hpp"
+
+int main()
+{
+    TA t1;
+    t1.setSupervisor("Dr. Caohuu");
+    t1.setId(55555);
+    t1.setTitle("Adjunct Prof.");
+    cout<<t1.getSupervisor()<<" "<<t1.getId()<<" "<<t1.getTitle();
+    return 0;
+}
