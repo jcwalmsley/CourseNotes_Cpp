0e46c97dd46083f3f961d63f14cd19d1c875b3ef
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-21 15:20:51 +0100

    UPDATE dir M2_multiple_inheritance

---
+++ M11_classes_inheritance/M0_intro/intro.cpp
@@ -0,0 +1,7 @@
+/*
+    In C++ we can derive a class from another class, called a base. The derived class can access members and functions of the base class.
+
+    We say the derived class has inherited from the base class.
+
+    In the image below we see a graphical representation of class inheritance. The base class is at the top of the tree and the derived class is below it.
+*/
