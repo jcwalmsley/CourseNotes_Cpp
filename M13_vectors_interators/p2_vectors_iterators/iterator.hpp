3fe28b3377ccb46f4c6b55b2b30c0459e097a42d
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-23 14:39:20 +0100

    REVISE  directory names

---
+++ M13_vectors_interators/p2_vectors_iterators/iterator.hpp
@@ -0,0 +1,14 @@
+// constructing vectors
+// constructing vectors
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printVector(vector<float> vIn);
+
+ void printVector(vector<float> vIn)
+ {//printing the contents of vIns
+    //TODO: Complete the function
+
+ }
