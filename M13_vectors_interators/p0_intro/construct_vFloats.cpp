82d7ecae3d06764ed339e1c5711659fca23e4fe8
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-23 14:25:26 +0100

    REVISE directory contents

---
+++ M13_vectors_interators/p0_intro/construct_vFloats.cpp
@@ -0,0 +1,19 @@
+/*Create a vector of floats called vFloat.
+* vFloat should have an iterator.
+*/
+
+//TODO: include the necessary libraries
+
+int main ()
+{
+ //TODO: instantiate vFloat vector
+
+ std::cout<<"vFloat has "<<vFloat.size()<<" elements\n";
+
+ //Changing the size of vectorInts to 6
+ //TODO: Change the size of vFloat to 10 elements
+
+ std::cout<<"\n\n vFloat now has "<<vFloat.size()<<" elements\n";
+
+ return 0;
+}
