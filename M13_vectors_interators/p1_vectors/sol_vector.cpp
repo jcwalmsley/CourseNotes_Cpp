0c0e4ace0c5a3bd0b7a20a67e8c72cdbc14a6283
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-23 10:47:39 +0100

    RECONFIGURE file organization within dir M13_vectors_iterators

---
+++ M13_vectors_interators/p1_vectors/sol_vector.cpp
@@ -0,0 +1,25 @@
+/*
+    The following is a sample solution for the Vector Programming Quiz.
+
+    Create a vector of floats called vFloat.
+    vFloat should have an iterator.
+*/
+
+//TODO: include the necessary libraries
+#include <iostream>
+#include <vector>
+
+
+int main ()
+{
+  //TODO: instantiate vFloat vector
+  std::vector<float> vFloat;
+  std::cout<<"vFloat has "<<vFloat.size()<<" elements\n";
+
+  //Changing the size of vectorInts to 6
+  //TODO: Change the size of vFloat to 10 elements
+  vFloat.resize(10);
+  std::cout<<"\n\n vFloat now has "<<vFloat.size()<<" elements\n";
+
+  return 0;
+}
