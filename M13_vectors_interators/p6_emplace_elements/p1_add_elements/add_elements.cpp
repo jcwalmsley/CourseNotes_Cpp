8874e135fc51340cc6b8577a54143bb7510766bf
James C Walmsley <jw.prof.cntrng@gmail.com>
2019-12-24 09:50:44 +0100

    ADD 4 files to dir p1_add_elements

---
+++ M13_vectors_interators/p6_emplace_elements/p1_add_elements/add_elements.cpp
@@ -0,0 +1,27 @@
+In this programming quiz, use these member functions to complete the functions in the header file.
+
+assign
+push_back
+insert
+emplace
+main.cpp
+main.hpp
+// Goal: pracitce constructing vectors
+// and their iterators
+// Call the vector vFloat
+// Call the iterator it
+
+
+#include "main.hpp"
+
+int main ()
+{
+  vector<int> vInts;
+  vInts.assign(10, 5);
+  printVector(vInts);
+  assignFunction(vInts, 1);
+  pushBackFunction(vInts, 2);
+  emplaceFunction(vInts,1, 3);
+
+  return 0;
+}
